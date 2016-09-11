56866bd0218bca0748a2174e5d231559b287c85e
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2016-09-10 23:14:57 -0400

    checked in vignette

---
+++ inst/doc/tutorial-2.R
@@ -0,0 +1,11 @@
+## ----setup0, include=FALSE-----------------------------------------------
+knitr::opts_chunk$set(prompt=FALSE, comment="")
+
+## ------------------------------------------------------------------------
+set.seed(3)
+rnorm(10)
+set.seed(3)
+rnorm(10)
+set.seed(4)  # set seed for different sequence of numbers
+rnorm(10)
+

1a116bf005153a171f697d4d8fab8b5f91dad144
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2016-09-10 23:13:50 -0400

    updates to vignettes

--- R/data.R
+++ R/data.R
@@ -10,22 +10,29 @@
 #'
 #' @param id the \code{id} to specify which dataset to return the filepath to.
 #' With no argument supplied, this returns a dataframe of all datasets.
+#' @param include_raw whether to include the raw files.
 #'
 #' @examples
 #' eve102_data()  # retrieve all datasets
 #' eve102_data(2)  # retrieve the path to the second dataset
 #' @export
-eve102_data <- function(id=NULL) {
+eve102_data <- function(id=NULL, include_raw=FALSE) {
   # Note: these are the raw files, will likely hide these from users later on
   owidth = options()$width
   on.exit({options(width=owidth)})
   options(width=120)
-  files <- c(
-             "CEU_10000.hw.gz", "CEU_YRI_10000.hw.gz", "YRI_10000.hw.gz")
+
+  # Registry of files with descriptions (TODO: can go to TSV file in
+  # inst/extdata later)
+  files <- c("CEU_10000.gz", "CEU_YRI_10000.gz", "YRI_10000.gz")
   desc <- c(
             "            10,000 HapMap SNPs, CEU individuals",
             "    10,000 HapMap SNPs, CEU and YRI individuals",
             "            10,000 HapMap SNPs, YRI individuals")
+  if (include_raw) {
+    files <- c(files, "CEU_10000.hw.gz", "CEU_YRI_10000.hw.gz", "YRI_10000.hw.gz")
+    desc <- c(desc, paste0(desc, ", raw"))
+  }
   ids = seq_along(files)
   data <- data.frame(id=ids, file=files, description=desc, stringsAsFactors=FALSE)
   data$path  <- sapply(data$file, function(x) 
---
+++ README.md
@@ -0,0 +1,5 @@
+## EVE102 R Package: R Course Materials for Population Genetics
+
+
+ - [R Tutorial, Part I]()
+ - [R Tutorial, Part II]()
